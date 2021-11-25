c9d025a42f066ccb7787fe3700e8ddb8c177a083
Juan Pablo Duque <juan-pablo-duque@hotmail.com>
2021-11-25 13:54:25 +0100

    changed static settings

--- italycoast/italycoast/settings.py
+++ italycoast/italycoast/settings.py
@@ -153,10 +153,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
