aab64bbf0b89f5fe97bbeb09404d390e58bbea9f
Juan Pablo Duque <juan-pablo-duque@hotmail.com>
2021-11-25 15:55:48 +0100

    added django heroku

--- italycoast/italycoast/settings.py
+++ italycoast/italycoast/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from django.core.wsgi import get_wsgi_application
 
+import django_heroku
+# Activate Django-Heroku.
+django_heroku.settings(locals())
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 #BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -164,3 +168,4 @@ PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
+
